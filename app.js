a02d131690761d4eae85471b14e32269f904418d
a7mad3akef <a7mad3akef@gmail.com>
2017-04-27 09:15:00 +0200

    add the current time to text mesage

--- app.js
+++ app.js
@@ -666,6 +666,10 @@ function sendJsonMessage(recipientId,keyword) {
  *
  */
 function sendTextMessage(recipientId, messageText) {
+  var dateTime = require('node-datetime');
+  var dt = dateTime.create();
+  var formatted = dt.format('Y-m-d H:M:S');
+  messageText = messageText + formatted ;
   var messageData = {
     "recipient": {
       "id": recipientId
